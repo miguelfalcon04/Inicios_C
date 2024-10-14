0f50595d0ce802c8a2d58e44986fa259978b036b
miguelfalcon04 <falconmiguel.2004@gmail.com>
2024-10-14 12:52:36 +0200

    Ejercicios del 1 al 3

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -8,9 +8,84 @@ namespace HolaMundo
 {
     internal class Program
     {
+        static void piramide()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            while (n < 0)
+            {
+                Console.WriteLine("n debe ser mayor o igual a 0");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int k = 0; k < n - i; k++)
+                {
+                    Console.Write("*");
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static void eje01()
+        {
+            Console.Write("Introduzca un número: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduzca otro número: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            while (n2 == 0) { 
+                Console.WriteLine("Este número debe ser distinto de 0");
+                Console.Write("Introduzca otro número: ");
+                n2 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("La suma es " + (n1+n2));
+            Console.WriteLine("La resta es " + (n1 - n2));
+            Console.WriteLine("La multiplicación es " + (n1 * n2));
+            Console.WriteLine("La división es " + (n1 / n2));
+            Console.WriteLine("El módulo es " + (n1 % n2));
+        }
+        static void eje02() {
+            const double precioHamburguesa = 10.5;
+            const double precioPatata = 1.7;
+            const double precioBebida = 2.6;
+
+            Console.Write("Cantidad de hamburguesas: ");
+            int nHam = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Cantidad de papas: ");
+            int nPapa = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Cantidad de bebidas: ");
+            int nBebida = Convert.ToInt32(Console.ReadLine());
+
+            double total = (precioHamburguesa * nHam) + (precioPatata * nPapa) + (precioBebida * nBebida);
+            Console.WriteLine("Cantidad a pagar: " + total + " $");
+        }
+
+        static void eje03() {
+            Console.Write("Introduce un número: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Valor absoluto: " + (Math.Abs(n)));
+            Console.WriteLine("Potencia al cubo: " + (Math.Pow(n, 3)));
+            Console.WriteLine("Raíz cuadrada: " + (Math.Sqrt(n)));
+            Console.WriteLine("Raíz cuadrada: " + (Math.Sqrt(n)));
+            Console.WriteLine("Seno: " + (Math.Sin(n)));
+            Console.WriteLine("Coseno: " + (Math.Cos(n)));
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            eje03();
         }
     }
 }
