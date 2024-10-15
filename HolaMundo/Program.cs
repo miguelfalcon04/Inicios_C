909d1f5ace0521cc5887728f39faa18b617dc1ab
Miguel <falconmiguel.2004@gmail.com>
2024-10-15 19:12:05 +0200

    Excercises Solved

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -8,32 +8,6 @@ namespace HolaMundo
 {
     internal class Program
     {
-        static void piramide()
-        {
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            while (n < 0)
-            {
-                Console.WriteLine("n debe ser mayor o igual a 0");
-                n = Convert.ToInt32(Console.ReadLine());
-            }
-
-            for (int i = 0; i < n; i++)
-            {
-                for (int k = 0; k < n - i; k++)
-                {
-                    Console.Write("*");
-                }
-
-                for (int j = 0; j < i; j++)
-                {
-                    Console.Write(" ");
-                }
-
-                Console.WriteLine();
-            }
-        }
-
         static void eje01()
         {
             Console.Write("Introduzca un número: ");
@@ -135,9 +109,86 @@ namespace HolaMundo
             Console.ReadLine();
         }
 
+        static void eje07() {
+            Console.Write("¿Cuántos cuesta el producto?: ");
+            int price = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("¿Cuántos productos llevará?: ");
+            int product = Convert.ToInt32(Console.ReadLine());
+
+            int total = price * product;
+
+            Console.WriteLine($"El coste total es de {total}");
+            Console.ReadLine();
+        }
+
+        static void eje08() {
+            double parcial1, parcial2, examen, trabajoFinal, notaFinal;
+
+            Console.Write("Introduce la calificación del primer examen: ");
+            parcial1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce la calificación del segundo examen: ");
+            parcial2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce la calificación del examen: ");
+            examen = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce la calificación del trabajo final: ");
+            trabajoFinal = Convert.ToDouble(Console.ReadLine());
+
+            double promedioParciales = (parcial1 + parcial2) / 2;
+            notaFinal = (promedioParciales * 0.50) + (examen * 0.30) + (trabajoFinal * 0.20);
+
+            Console.WriteLine("\nTu calificación es: " + notaFinal);
+            Console.ReadLine();
+        }
+
+        static void eje09() {
+            double aporte1, aporte2, aporte3, totalAporte;
+
+            Console.Write("Introduce la aportación del socio 1: ");
+            aporte1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce la aportación del socio 2: ");
+            aporte2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Introduce la aportación del socio 3: ");
+            aporte3 = Convert.ToDouble(Console.ReadLine());
+
+            totalAporte = aporte1 + aporte2 + aporte3;
+
+            double porcentaje1 = (aporte1 / totalAporte) * 100;
+            double porcentaje2 = (aporte2 / totalAporte) * 100;
+            double porcentaje3 = (aporte3 / totalAporte) * 100;
+
+            Console.WriteLine($"El porcentaje del socio 1 es {porcentaje1}, del socio 2 es {porcentaje2}, y del socio 3 es {porcentaje3}");
+            Console.ReadLine();
+        }
+
+        static bool eje10() {
+            bool inicio = false;
+            string letras = "";
+
+            Console.Write("Introduzca su dirección web: ");
+            string web = Console.ReadLine();
+
+            for(int i = 0; i < 3; i++) {
+                letras += web[i];
+            }
+
+            if (letras.Equals("www")) {
+                inicio = true;
+            }
+
+            Console.Write(inicio);
+            Console.ReadLine();
+            return inicio;
+        }
+
         static void Main(string[] args)
         {
-            eje06();
+            eje01();
         }
     }
 }
